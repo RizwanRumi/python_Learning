bd7eedb4c4625e48469726af16d211acaaea9abc
RizwanRumi <rizwan.cse33@gmail.com>
2021-02-27 23:28:44 +0100

    add Raising Custom Exceptions example

--- OOP/python_raise_exception.py
+++ OOP/python_raise_exception.py
@@ -1,3 +1,10 @@
+class CoffeeTooHotException(Exception):
+    def __init__(self, msg):
+        super().__init__(msg)
+class CoffeeTooColdException(Exception):
+    def __init__(self, msg):
+        super().__init__(msg)
+
 class CoffeCup:
     def __init__(self, temperature):
         self.__temperature = temperature
@@ -5,10 +12,11 @@ class CoffeCup:
     def drink_coffee(self):
         if self.__temperature > 85:
             #print('Coffee Too Hot ')
-            raise Exception('Coffee Too Hot ')
+            #raise Exception('Coffee Too Hot ')
+            raise CoffeeTooHotException('Coffee temperature: ' + str(self.__temperature))
         elif self.__temperature < 65:
             # print('Coffee Too Cold ')
-            raise Exception('Coffee Too Cold ')
+            raise CoffeeTooColdException('Coffee temperature: ' + str(self.__temperature))
         else:
             print('Coffee OK to Drink ')
 
