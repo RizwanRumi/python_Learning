9dda30a7154f67266b123f2cdb303a151bf33af9
RizwanRumi <rizwan.cse33@gmail.com>
2021-02-27 23:59:22 +0100

    add example of using with keyword for a file operation

--- OOP/file_operation.py
+++ OOP/file_operation.py
@@ -1,3 +1,4 @@
+'''
 fh = open('demo.txt', 'a')
 
 try:
@@ -5,3 +6,10 @@ try:
         fh.write("this is line no %d\n" % (i+100))
 finally:
     fh.close()
+'''
+
+# use 'with' keyword instead of explicit of previous code which has used try and finally
+
+with open('demo.txt', 'a') as fh:
+    for i in range(10):
+        fh.write("this is line no %d\n" % (i+200))
