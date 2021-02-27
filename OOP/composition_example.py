769afdb3bbe9c1f67e7c3ea916b42b044fd0eaeb
RizwanRumi <rizwan.cse33@gmail.com>
2021-02-27 16:54:12 +0100

    add composition example

---
+++ OOP/composition_example.py
@@ -0,0 +1,23 @@
+'''
+composition represents 'part-of' relationships
+'''
+
+class Salary:
+    def __init__(self, pay, bonus):
+        self.pay = pay
+        self.bonus = bonus
+
+    def annual_salary(self):
+        return (self.pay*12) + self.bonus
+
+class Employee:
+    def __init__(self, name, age, pay, bonus):
+        self.name = name
+        self.age = age
+        self.obj_salary = Salary(pay, bonus)
+
+    def total_salary(self):
+        return self.obj_salary.annual_salary()
+
+emp = Employee('max', 26, 15000, 10000)
+print(emp.total_salary())
