2b5df60245d9e703151474267d595f4e4eaaaf95
RizwanRumi <rizwan.cse33@gmail.com>
2021-02-27 23:04:40 +0100

    else and finally statement has been added

--- OOP/exception.py
+++ OOP/exception.py
@@ -5,12 +5,14 @@ b = float(input('Number 2: '))
 
 try:
     result = a / b
-except Exception as e:
-    print("Error", e)
 except ZeroDivisionError as e:
     print("ZeroDisionError: ", type(e))
 except TypeError as e:
     print("TypeError: ", type(e))
+else:
+    print('__else__')
+finally:
+    print('__Finally__')
 
 print('Result = ', result)
 print('End')
