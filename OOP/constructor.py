3c5e70fd6cd5e74ab544d6bac578bbcefb4884c5
RizwanRumi <rizwan.cse33@gmail.com>
2021-02-27 22:05:18 +0100

    Revert "...."
    This reverts commit 6d14f225efd75874053bc9f5e6746605e77e407a.

--- OOP/constructor.py
+++ OOP/constructor.py
@@ -5,7 +5,8 @@ different actions on different parameters.
 python does not support explicit multiple constructors, yet there
 are some ways using which the multiple constructors can be achieved
 
-1) Overloading constructors based on arguments
+1) Overloading constructors based on arguments (*args -> tuple,
+**args -> dictionary)
 2) Calling methods from __init__
 3) Using @Classmethod decorator
 '''
