9735b75797db6cce6fb76a05d608eb04f9a26085
RizwanRumi <rizwan.cse33@gmail.com>
2021-02-27 21:55:45 +0100

    ....

--- OOP/constructor.py
+++ OOP/constructor.py
@@ -5,8 +5,7 @@ different actions on different parameters.
 python does not support explicit multiple constructors, yet there
 are some ways using which the multiple constructors can be achieved
 
-1) Overloading constructors based on arguments (*args -> tuple,
-**args -> dictionary)
+1) Overloading constructors based on arguments
 2) Calling methods from __init__
 3) Using @Classmethod decorator
 '''
